5446521120e65d032a11bec2878d6b46c4105722
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-04-02 21:12:44 +0200

    test: create routes test

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,5 +1,19 @@
+import { envs } from "../config/envs";
+import { Server } from "../presentation/server";
+
+jest.mock("../presentation/server");
+
 describe("Testing App.ts", () => {
-  test("should work", () => {
-    expect(true).toBeTruthy();
+  test("should call server with arguments and start", async () => {
+    await import("../app");
+
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      public_path: envs.PUBLIC_PATH,
+      routes: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenLastCalledWith();
   });
 });
---
+++ src/test/presentation/todos/routes.test.ts
@@ -0,0 +1,48 @@
+import request from "supertest";
+import { testServer } from "../../test-server";
+import { prisma } from "../../../data/postgres";
+
+describe("routes.ts", () => {
+  beforeAll(async () => {
+    await testServer.start();
+  });
+
+  afterAll(async () => {
+    testServer.close();
+  });
+
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
+  const todo1 = { text: "Hola Mundo 1" };
+  const todo2 = { text: "Hola mMndo 2" };
+
+  test("should return TODOs /api/todos", async () => {
+    await prisma.todo.createMany({ data: [todo1, todo2] });
+
+    const { body } = await request(testServer.app)
+      .get("/api/todos")
+      .expect(200);
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeNull();
+  });
+
+  test("should return a TODO api/todos/:id", async () => {
+    const todo = await prisma.todo.create({ data: todo1 });
+
+    const { body } = await request(testServer.app)
+      .get(`/api/todos/${todo.id}`)
+      .expect(200);
+
+    expect(body).toEqual({
+      id: todo.id,
+      text: todo1.text,
+      completedAt: null,
+    });
+  });
+});
